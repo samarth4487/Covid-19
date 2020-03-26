580896a59b369d514018cbe9dd1f21c323759792
samarth4487 <samarthpaboowal@yahoo.co.in>
2020-03-26 17:22:56 +0530

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Covid-19
+//
+//  Created by Samarth Paboowal on 26/03/20.
+//  Copyright © 2020 Samarth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
